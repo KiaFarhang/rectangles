d5d82b12cb39fdb35b20f2093574644054091bae
Kia Farhang <kfarhang0@gmail.com>
2022-06-21 08:12:18 -0500

    feature: demonstrate adjacency

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	demonstrateContainment()
+	demonstrateAdjacency()
 }
 
 func demonstrateContainment() {
@@ -19,6 +20,19 @@ func demonstrateContainment() {
 	fmt.Printf("Rectangle A: %v\nRectangle B: %v\n", rectangleA, rectangleB)
 	fmt.Printf("A contains B: %t\n", rectangleA.Contains(rectangleB))
 	fmt.Printf("B contains A: %t\n", rectangleB.Contains(rectangleA))
+}
+
+func demonstrateAdjacency() {
+	rectangleA := buildRectangle(shapes.Point{X: 2, Y: 4}, shapes.Point{X: 5, Y: 2})
+	adjacentRectangle := buildRectangle(shapes.Point{X: 2, Y: 2}, shapes.Point{X: 3, Y: 0})
+
+	fmt.Println("Adjacency demonstration:")
+	fmt.Printf("Rectangle A: %v\n Rectangle B: %v\n", rectangleA, adjacentRectangle)
+	fmt.Printf("Are rectangles adjacent: %t\n", rectangleA.Adjacent(adjacentRectangle))
+
+	nonAdjacentRectangle := buildRectangle(shapes.Point{X: 10, Y: 10}, shapes.Point{X: 14, Y: 7})
+	fmt.Printf("Rectangle A: %v\n Rectangle C: %v\n", rectangleA, nonAdjacentRectangle)
+	fmt.Printf("Are rectangles adjacent: %t\n", rectangleA.Adjacent(nonAdjacentRectangle))
 }
 
 func buildRectangle(topLeft, bottomRight shapes.Point) *shapes.Rectangle {
